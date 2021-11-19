413004efa4a0cf89fecf0eda5b15cea3078cf69f
Tigulima <62662386+tigulima@users.noreply.github.com>
2021-11-19 15:43:49 -0300

    Update ex02
    Adding comments

--- ex02.py
+++ ex02.py
@@ -6,6 +6,7 @@ url = input("Enter a URL: ")
 driver = webdriver.Chrome("C:/Program Files (x86)/ChromeDriver/chromedriver.exe")
 driver.get(url)
 
+# The Email_Regex I'm using was found on this site: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
 EMAIL_REGEX = r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'''
 
 list = []
